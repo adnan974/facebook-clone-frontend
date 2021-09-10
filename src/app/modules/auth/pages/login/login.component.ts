c0ff64700d4b3b7682c565e4c3c0e248c698f043
Adnan <adnan.goulamaly@gmail.com>
2021-09-10 11:25:54 +0400

    update: modification du style de la page login pour qu'elle ressemble à celle de facebook

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  public projectName:string = environment.projectName
 
   constructor(private http:HttpClient) { }
 
